f9ff5ddc266c525781981bc6bb0b8fb8f737512d
Keri Clowes <kerilyn.clowes@gmail.com>
2014-03-21 11:29:06 -0600

    Able to filter items list with query string
    Signed-off-by: Scott Skender <sjskender@gmail.com>

--- app.rb
+++ app.rb
@@ -2,11 +2,13 @@ require 'sinatra'
 
 require './lib/item'
 require './lib/item_repository'
+require './lib/filter'
 
 get '/' do
   erb :index
 end
 
 get '/items' do
+  @filter = params[:filter]
   erb :items_list
 end
--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -2,4 +2,7 @@ class ItemRepository
   def items
     [ Item.new("Cuppa Joe", 1), Item.new("Cup of Tea", 2), Item.new("Cup of Pizza", 3) ]
   end
+  def save
+
+  end
 end
