3c38dc8077fa86e3855b0bdf062fa1d851a61e56
Keri Clowes <kerilyn.clowes@gmail.com>
2014-03-21 15:28:59 -0600

    Resolved method error in Heroku
    Signed-off-by: Scott Skender <sjskender@gmail.com>

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,15 +1,14 @@
 class ItemRepository
+  attr_reader :item_list
+
   def initialize
     @item_list = [ Item.new("Cuppa Joe", 1), Item.new("Cup of Tea", 2), Item.new("Cup of Pizza", 3) ]
-  end
-
-  def self.list_items
-    @item_list
   end
 
   def add_items(item_to_add=nil)
     if item_to_add != nil
     @item_list << Item.new(item_to_add, 4)
     end
+    @item_list
    end
 end
